a0fe96ceb0ec48bc25af7612ef559b41ac7c7e15
Lwin Maung Phyo <lwinmgphyo95@gmail.com>
2021-02-04 23:38:05 +0800

    Added button navigation with 3 fragments

---
+++ app/src/main/java/com/example/trippi/Hotel.java
@@ -0,0 +1,28 @@
+package com.example.trippi;
+
+import java.io.Serializable;
+
+public class Hotel implements Serializable {
+
+        public int id;
+        public String image_url;
+        public String name;
+        public String location;
+        public float lat;
+        public float lng;
+        public float rating;
+
+        public Hotel() {
+        }
+
+        public Hotel(int id, String image_url, String name, String location, float lat, float lng, float rating) {
+            this.id = id;
+            this.image_url = image_url;
+            this.name = name;
+            this.location = location;
+            this.lat = lat;
+            this.lng = lng;
+            this.rating = rating;
+        }
+
+}
--- app/src/main/java/com/example/trippi/MainActivity.java
+++ app/src/main/java/com/example/trippi/MainActivity.java
@@ -1,14 +1,96 @@
 package com.example.trippi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView; // declare bottom navigation view
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestLocationPermission();
+
+        bottomNavigationView = findViewById(R.id.bottomNaivgationView); // initialize the bottom navigation
+        bottomNavigationView.setSelectedItemId(R.id.bottomNavigationHotelIcon); // set default bottom navigation icon
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+        loadFragment(HotelFragment.newInstance("", ""));
+
+    }
+
+    private void loadFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentFrame, fragment).addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    //     Navigation icon click configuration
+    BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()){
+                        case R.id.bottomNavigationHotelIcon:
+                            loadFragment(HotelFragment.newInstance("", ""));
+                            return true;
+                        case R.id.bottomNavigationMessageIcon:
+                            loadFragment(MessageFragment.newInstance("", ""));
+                            return true;
+                        case R.id.bottomNavigationProfileIcon:
+                            loadFragment(ProfileFragment.newInstance("", ""));
+                            return true;
+                    }
+                    return true;
+                }
+            };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbar_menu, menu);
+        return true;
+    }
+
+    public void requestLocationPermission() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)){
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }else{
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults){
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
